2e7f101b061b920d5d4ff000be54c0f570925a49
vgotu99 <vgotu99@gmail.com>
2025-08-31 20:14:13 +0900

    feat: add LogoImage component for improve logo display in NextraNavbar

---
+++ components/LogoImage.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import { useTheme } from "nextra-theme-docs";
+
+const LogoImage = () => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Image
+      src={
+        resolvedTheme === "dark"
+          ? "/docs-logo-dark.svg"
+          : "/docs-logo-light.svg"
+      }
+      alt="NEXT-NEXUS"
+      width={200}
+      height={48}
+    />
+  );
+};
+
+export default LogoImage;
--- components/NextraNavbar.tsx
+++ components/NextraNavbar.tsx
@@ -1,25 +1,10 @@
-"use client";
-
-import { Navbar, useTheme } from "nextra-theme-docs";
-import { Image } from "nextra/components";
+import LogoImage from "@/components/LogoImage";
+import { Navbar } from "nextra-theme-docs";
 
 const NextraNavbar = ({ locale }: { locale: "/en" | "/ko" }) => {
-  const { resolvedTheme } = useTheme();
-
   return (
     <Navbar
-      logo={
-        <Image
-          src={
-            resolvedTheme === "dark"
-              ? "/docs-logo-dark.svg"
-              : "/docs-logo-light.svg"
-          }
-          alt="NEXT-NEXUS"
-          width={200}
-          height={48}
-        />
-      }
+      logo={<LogoImage />}
       logoLink={locale}
       projectLink="https://github.com/vgotu99/next-nexus"
     />
